8b141eb557f33bb715dc6bcbfeae497a7a575420
Hiroaki Nakamura <hnakamur@gmail.com>
2015-08-25 01:30:46 +0900

    Implement server-side pagination

--- project/api/views.py
+++ project/api/views.py
@@ -4,13 +4,15 @@ from django.shortcuts import render
 from bookmarks.models import Bookmark
 
 def bookmarks(request):
-    bookmarks = Bookmark.objects.all()[:10]
+    bookmarks = Bookmark.objects.all()
+    offset = int(request.GET.get('offset') or '0')
+    limit = int(request.GET.get('limit') or len(bookmarks) - offset)
     rows = [{
         'id': b.id,
         'url': b.url,
         'title': b.title,
         'bookmarked_at': b.bookmarked_at
-    } for b in bookmarks]
+    } for b in bookmarks[offset:offset + limit]]
     data = {
         'total': len(bookmarks),
         'rows': rows
