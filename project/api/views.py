41aca15d2a3784d760a148850959df11cb2c4c34
Hiroaki Nakamura <hnakamur@gmail.com>
2015-08-25 13:41:14 +0900

    Implement bookmark registration feature

--- project/api/views.py
+++ project/api/views.py
@@ -7,30 +7,43 @@ from bookmarks.models import Bookmark
 
 logger = getLogger(__name__)
 
+@csrf_exempt
 def bookmarks(request):
-    bookmarks = Bookmark.objects.all()
-    offset = int(request.GET.get('offset') or '0')
-    limit = int(request.GET.get('limit') or len(bookmarks) - offset)
-    rows = [{
-        'id': b.id,
-        'url': b.url,
-        'title': b.title,
-        'bookmarked_at': b.bookmarked_at
-    } for b in bookmarks[offset:offset + limit]]
-    data = {
-        'total': len(bookmarks),
-        'rows': rows
-    }
-    return JsonResponse(data)
+    if request.method == 'GET':
+        bookmarks = Bookmark.objects.all()
+        offset = int(request.GET.get('offset') or '0')
+        limit = int(request.GET.get('limit') or len(bookmarks) - offset)
+        rows = [{
+            'id': b.id,
+            'url': b.url,
+            'title': b.title,
+            'bookmarked_at': b.bookmarked_at
+        } for b in bookmarks[offset:offset + limit]]
+        data = {
+            'total': len(bookmarks),
+            'rows': rows
+        }
+        return JsonResponse(data)
+    elif request.method == 'POST':
+        url = request.POST['url']
+        title = request.POST['title']
+        bookmark = Bookmark()
+        bookmark.url = url
+        bookmark.title = title
+        bookmark.save()
+        logger.debug('create bookmark for url={0}, title={1}'.format(url, title))
+        data = {}
+        return JsonResponse(data, status=201)
+    else:
+        return JsonResponse({}, status=405)
 
 @csrf_exempt
 def bookmark(request, bookmark_id):
-    data = {}
     if request.method == 'DELETE':
         try:
             bookmark = Bookmark.objects.get(pk=bookmark_id)
             bookmark.delete()
-            return JsonResponse(data, status=204)
+            return JsonResponse({}, status=204)
         except:
             data = {
                 'errors': [{
@@ -38,5 +51,6 @@ def bookmark(request, bookmark_id):
                 }]
             }
             return JsonResponse(data, status=500)
-    return JsonResponse(data)
+    else:
+        return JsonResponse({}, status=405)
 
--- project/bookmarks/models.py
+++ project/bookmarks/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Bookmark(models.Model):
     url = models.TextField(unique=True)
     title = models.TextField()
-    bookmarked_at = models.DateTimeField()
+    bookmarked_at = models.DateTimeField(auto_now_add=True)
