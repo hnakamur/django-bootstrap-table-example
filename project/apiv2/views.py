810961f65c37d27c5e2d99cf102064d0b4e300f3
Hiroaki Nakamura <hnakamur@gmail.com>
2015-08-26 23:31:43 +0900

    Use ListCreateAPIView as base class to support bookmark creation

--- project/apiv2/views.py
+++ project/apiv2/views.py
@@ -1,14 +1,13 @@
 from django.db.models import Q
 from django.shortcuts import render
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.generics import ListAPIView
 from rest_framework_json_api.renderers import JSONRenderer
-from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from bookmarks.models import Bookmark
 from bookmarks.serializers import BookmarkSerializer
 
-class BookmarkListCreateAPIView(ListAPIView):
+class BookmarkListCreateAPIView(ListCreateAPIView):
     queryset = Bookmark.objects.all()
     serializer_class = BookmarkSerializer
     resource_name = 'bookmark'
