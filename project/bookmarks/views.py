ecbe8379cfa211aa55dd0bd31c89e81cd7469c25
Hiroaki Nakamura <hnakamur@gmail.com>
2015-08-28 15:25:40 +0900

    Clean up bookmarks/views.py

--- project/bookmarks/views.py
+++ project/bookmarks/views.py
@@ -1,8 +1,4 @@
-from django.http import JsonResponse
 from django.shortcuts import render
 
-from .models import Bookmark
-
 def index(request):
-    context = {}
-    return render(request, 'bookmarks/index.html', context)
+    return render(request, 'bookmarks/index.html', {})
